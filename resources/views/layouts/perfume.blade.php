d2a5eb3986e49d514f4d1840ef55cef3dbea0ed3
hananhassona <hanannino13@gmail.com>
2022-01-19 11:35:58 +0200

    other update

--- resources/views/layouts/perfume.blade.php
+++ resources/views/layouts/perfume.blade.php
@@ -69,25 +69,25 @@
 																			</div>
 																</span>
 												</div>
-												<div class="stock">
+												<!-- <div class="stock">
 													<a href="#">In Stock</a>
+												</div> -->
+												<div class="perfume_heart">
+													<a class="action-btn" href="wishlist.html" data-toggle="tooltip" data-original-title="Add to Wishlist"><img src="{{asset('assets/front/images/heart.png')}}" class="img-fluid" alt="" /></a>
 												</div>
-												
 												<div class="description">
 													<p>Nunc facilisis sagittis ullamcorper. Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus. Sed et lorem nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean eleifend laoreet congue. Viva..</p>
 												</div>
 												<div class="price-box">
-													<span class="old-price">$105.00</span>
-													<span class="new-price">$90.00</span>
+													<span class="old-price">sold(23)</span>
+													<span class="new-price">SAR90.00</span>
 												</div>
-												<div class="qty">
-													<span class="choose-title">Qty : </span>
-													<input type="number" placeholder="1">
-												</div>
+												<div class="quantity buttons_added">
+	<input type="button" value="-" class="minus"><input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="" inputmode=""><input type="button" value="+" class="plus">
+</div>
+												
 												<div class="pro-actions">
 													<a class="action-btn action-btn-1" href="cart.html"><i class="pe-7s-cart"></i>Add to Cart</a>
-													<a class="action-btn" href="wishlist.html" data-toggle="tooltip" data-original-title="Add to Wishlist"><img src="{{asset('assets/front/images/heart.png')}}" class="img-fluid" alt="" /></a>
-													<a class="action-btn" href="#" data-toggle="tooltip" data-original-title="Compare"><i class="pe-7s-repeat"></i></a>
 												</div>
 											</div>
 											
@@ -99,13 +99,14 @@
 						<!--//banner-slider-->
 						<div class="right-banner">
 							<div class="right-1">
-							<a href="#" class="perfume"><img src="{{asset('assets/front/images/pexels-valeria-boltneva-1961795.png')}}" class="img-fluid" alt="" /></a>
+							<a href="#" class="perfume"><img src="{{asset('assets/front/images/pexels-valeria-boltneva-1961795')}}" class="img-fluid" alt="" /></a>
 							</div>
 						</div>
 					</div>
 </section>
 
 	  <script src="{{asset('assets/front/js/jquery-2.1.4.min.js')}}"></script>
+	  <script data-require="jquery@3.1.1" data-semver="3.1.1" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
       <script src="{{asset('assets/front/js/jquery-3.3.1.min.js')}}"></script>
 
 <!--/login-->
@@ -145,6 +146,31 @@
   $('[data-toggle="offcanvas"]').click(function () {
         $('#wrapper').toggleClass('toggled');
   });  
+});
+</script>
+<!-- quantity -->
+<script>
+	function wcqib_refresh_quantity_increments() {
+    jQuery("div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)").each(function(a, b) {
+        var c = jQuery(b);
+        c.addClass("buttons_added"), c.children().first().before('<input type="button" value="-" class="minus" />'), c.children().last().after('<input type="button" value="+" class="plus" />')
+    })
+}
+String.prototype.getDecimals || (String.prototype.getDecimals = function() {
+    var a = this,
+        b = ("" + a).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+    return b ? Math.max(0, (b[1] ? b[1].length : 0) - (b[2] ? +b[2] : 0)) : 0
+}), jQuery(document).ready(function() {
+    wcqib_refresh_quantity_increments()
+}), jQuery(document).on("updated_wc_div", function() {
+    wcqib_refresh_quantity_increments()
+}), jQuery(document).on("click", ".plus, .minus", function() {
+    var a = jQuery(this).closest(".quantity").find(".qty"),
+        b = parseFloat(a.val()),
+        c = parseFloat(a.attr("max")),
+        d = parseFloat(a.attr("min")),
+        e = a.attr("step");
+    b && "" !== b && "NaN" !== b || (b = 0), "" !== c && "NaN" !== c || (c = ""), "" !== d && "NaN" !== d || (d = 0), "any" !== e && "" !== e && void 0 !== e && "NaN" !== parseFloat(e) || (e = 1), jQuery(this).is(".plus") ? c && b >= c ? a.val(c) : a.val((b + parseFloat(e)).toFixed(e.getDecimals())) : d && b <= d ? a.val(d) : b > 0 && a.val((b - parseFloat(e)).toFixed(e.getDecimals())), a.trigger("change")
 });
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.js"></script>
